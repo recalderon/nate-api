e832988b7d31748d9ce976f039c2a86a70237472
Ricardo Calderon <ricardocalderon@apoioprodesp.sp.gov.br>
2022-11-15 21:17:27 -0300

    image struggle

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: { domains: [ "https://recalderon.github.io/", ], }
 }
 
 module.exports = nextConfig
