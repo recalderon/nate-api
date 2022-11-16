137265525774c154719427940b92c9a4a8348c38
Ricardo Calderon <ricardocalderon@apoioprodesp.sp.gov.br>
2022-11-15 22:28:06 -0300

    l

--- public/assets/dist/js/main.js
+++ public/assets/dist/js/main.js
@@ -22,6 +22,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
         aceitamusica.addEventListener('click', (event) =>{
             player.play();
             player2.play();
+            myModal.hide();
         })
     };
     
